aff3dbd7f9f21c56120dbe8d446324279e9240f3
alfiehe(何俊飞) <alfiehe@tencent.com>
2021-04-01 11:43:20 +0800

    目录调整一下

--- tests/unit/components/Loading.spec.js
+++ tests/unit/components/Loading.spec.js
@@ -1,22 +1,29 @@
+/**
+ * 测试一个 Loading 组件
+ */
 import { shallowMount } from '@vue/test-utils'
 import Loading from '@/components/Loading.vue'
 
 describe('Loading.vue', () => {
+  // 声明一个包裹器变量
+  let wrapper;
 
-  let wrapper
   // 测试用例之前，重新挂载组件，避免测试用例互相影响
   beforeEach(() => {
-    wrapper = shallowMount(Loading)
-  })
+    wrapper = shallowMount(Loading);
+  });
 
-  it('show func', async () => {
+  it('执行 show 方法后，应该显示loading DOM', async () => {
+
     expect(wrapper.vm.loadingText).toBe("")
     expect(wrapper.vm.showLoading).toBe(false)
     expect(wrapper.isVisible()).toBe(false)
 
     wrapper.vm.show();
-    // 触发了dom更新，需要调用 $nextTick()
+
+    // 触发了dom更新，需要调用一下组件实例的 $nextTick 方法
     await wrapper.vm.$nextTick()
+
     expect(wrapper.vm.showLoading).toBe(true)
     expect(wrapper.isVisible()).toBe(true)
   })
