fc055b5a0dc9f89283d5861f045765f32055fd15
alfiehe(何俊飞) <alfiehe@tencent.com>
2021-04-08 10:44:07 +0800

    监听子组件事件

--- src/router/index.js
+++ src/router/index.js
@@ -19,6 +19,11 @@ const routes = [
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   },
   {
+    path: '/demo',
+    name: 'Demo',
+    component: () => import('../views/Demo.vue')
+  },
+  {
     path: '/todo',
     name: 'Todo',
     component: () => import('../views/Todo.vue')
---
+++ src/views/Demo.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="demo">
+    <Blog />
+  </div>
+</template>
+
+<script>
+import Blog from '@/components/blog/blog.vue'
+
+export default {
+  name: 'Demo',
+  components: {
+    Blog
+  }
+}
+</script>
